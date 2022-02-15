f69cf81bfb9f9cca9e90bdfe7f2c88a7ff52af23
Aidan Gerber <agentaidan@gmail.com>
2022-02-14 20:57:25 -0600

    add octocat for gh logo

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,35 +1,41 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+    import Nav from '../components/Nav.svelte'
 </script>
 
 <Nav />
 <div class="container">
-	<h1>Paper Trading</h1>
-	<h2>Created by Aidan Gerber</h2>
-	<p>A game to paper trade different stocks with fake money. Created with Sveltekit.</p>
-	<p>
-		A new interpretation of the <a href="https://www.tradethisandthat.com/items/" target="_blank"
-			>Trade This and That algorithm</a
-		>
-	</p>
-	<p>
-		<a href="https://github.com/AidanG1/paper-trade" target="_blank"
-			>Source code <img
-				class="github-logo"
-				src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Flarge%2F2x%2Fgithub-icon-logo-png-transparent.png&f=1&nofb=1"
-				alt="Github Logo"
-				width="50"
-				height="50"
-			/></a
-		>
-	</p>
-	<p>
-		Inspired by <a href="https://paper-trader.davjhan.com/" target="_blank">David Han's game</a>
-	</p>
+    <h1>Paper Trading</h1>
+    <h2>Created by Aidan Gerber</h2>
+    <p>
+        A game to paper trade different stocks with fake money. Created with
+        Sveltekit.
+    </p>
+    <p>
+        A new interpretation of the <a
+            href="https://www.tradethisandthat.com/items/"
+            target="_blank">Trade This and That algorithm</a
+        >
+    </p>
+    <p>
+        <a href="https://github.com/AidanG1/paper-trade" target="_blank"
+            >Source code <img
+                class="github-logo"
+                src="/github-octocat.svg"
+                alt="Github Logo"
+                width="50"
+                height="50"
+            /></a
+        >
+    </p>
+    <p>
+        Inspired by <a href="https://paper-trader.davjhan.com/" target="_blank"
+            >David Han's game</a
+        >
+    </p>
 </div>
 
 <style>
-	.github-logo {
-		display: inline-block;
-	}
+    .github-logo {
+        display: inline-block;
+    }
 </style>
